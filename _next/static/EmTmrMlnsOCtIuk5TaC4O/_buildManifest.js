43fb10251c7d7debcce2b14fd7c1013fc5faa691
zhuojg <zhuojg@users.noreply.github.com>
2022-04-16 12:32:35 +0000

    deploy: aafb2c4ec79c2c28643850ef30a5c1d1c235cfd7

---
+++ _next/static/EmTmrMlnsOCtIuk5TaC4O/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0e226fb0-0ddc2b875c84ae07.js","static/chunks/pages/index-d8750619667ffe2c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
